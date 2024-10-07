bda75092df5920ab1b4f465615199acfaf174c04
Luiz Gustavo <gutfoda@gmail.com>
2024-10-07 11:11:34 -0300

    Começando os trabalhos na primeira parte da Homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
             {/* Cabeçalho */}
             <header>
                 <div className="logo">
-                    <img src="sua-logo.png" alt="Logo" /> {/* Insira o caminho da logo */}
+                    <img src="" alt="Logo" /> {/* Insira o caminho da logo */}
                 </div>
                 <div className="right-content">
                     <nav>
@@ -21,24 +21,18 @@ const Homepage = () => {
                         <button className="login-btn">Registrar</button>
                         <button className="register-btn">Entrar</button>
                     </div>
-
                 </div>
-                
             </header>   
             
             {/* Frase de impacto */}
             <section className="impact-phrase">
                 <p>Descubra o futuro da mobilidade sustentável</p>
-            </section>
-            
-            <h1>Localização em tempo real</h1>
-            <p>
-                        Com a nossa tecnologia de localização em tempo real, você nunca mais ficará sem energia no seu veículo elétrico. Basta acessar o nosso site para visualizar os postos de carregamento disponíveis mais próximos de você, com informações atualizadas sobre disponibilidade de vagas e preços.
-            </p>
-
-            
-
+            </section> 
         </div>
+
+        
+            
+
     );
 }
 
